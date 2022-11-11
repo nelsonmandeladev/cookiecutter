f5aee5e581824a255f41484e7b4356129136cbe1
nelsonmandeladev <patricknelson1995@gmail.com>
2022-11-11 07:59:57 +0100

    ngnix

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="p9lbUDAJKvxtShVyW8DXKZboBZAJApYqWmiF5V5WQevxvW0E4KlFUCD7KWTyBZ56",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "localhost.local"]
 
 # CACHES
 # ------------------------------------------------------------------------------
